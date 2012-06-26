8e2c798a251159e53a21666da8725c06fd92e4a0
bgriswold <bgriswold@gmail.com>
2012-06-26 08:55:52 -0700

    Db4o initial implementation

---
+++ SharpRepository.Db4o/Db4ORepositoryBase.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Linq;
+using Db4objects.Db4o;
+using Db4objects.Db4o.Linq;
+using SharpRepository.Repository;
+using SharpRepository.Repository.Caching;
+using SharpRepository.Repository.FetchStrategies;
+
+namespace SharpRepository.Db4o
+{
+    public class Db4oRepositoryBase<T, TKey> : LinqRepositoryBase<T, TKey> where T : class, new()
+    {
+        //Nice reference: http://jamesrhicks.blogspot.com/2010/02/getting-started-with-db4o.html
+        private IObjectContainer _documentStore;
+
+        internal Db4oRepositoryBase(string storagePath, ICachingStrategy<T, TKey> cachingStrategy = null)
+            : base(cachingStrategy)
+        {
+            Initialize(storagePath);
+        }
+
+        private void Initialize(string storagePath)
+        {
+            _documentStore = Db4oEmbedded.OpenFile(Db4oEmbedded.NewConfiguration(), storagePath);
+        }
+
+        protected override IQueryable<T> BaseQuery(IFetchStrategy<T> fetchStrategy = null)
+        {
+            return _documentStore.AsQueryable<T>();
+        }
+
+        protected override T GetQuery(TKey key)
+        {
+            return _documentStore.AsQueryable<T>().FirstOrDefault(x => MatchOnPrimaryKey(x, key));
+        }
+
+        private bool MatchOnPrimaryKey(T item, TKey keyValue)
+        {
+            TKey value;
+            return GetPrimaryKey(item, out value) && keyValue.Equals(value);
+        }
+
+        protected override void AddItem(T entity)
+        {
+            TKey id;
+
+            if (GetPrimaryKey(entity, out id) && Equals(id, default(TKey)))
+            {
+                id = GeneratePrimaryKey();
+                SetPrimaryKey(entity, id);
+            }
+
+            _documentStore.Store(entity);
+        }
+
+        protected override void DeleteItem(T entity)
+        {
+            _documentStore.Delete(entity);
+        }
+
+        protected override void UpdateItem(T entity)
+        {
+            _documentStore.Store(entity);
+        }
+
+        protected override void SaveChanges()
+        {
+            _documentStore.Commit();
+        }
+
+        public override void Dispose()
+        {
+            _documentStore.Close();
+            if (_documentStore != null)
+                _documentStore.Dispose();
+        }
+
+        private TKey GeneratePrimaryKey()
+        {
+            if (typeof (TKey) == typeof (Guid))
+            {
+                return (TKey) Convert.ChangeType(Guid.NewGuid(), typeof (TKey));
+            }
+
+            if (typeof (TKey) == typeof (Int32))
+            {
+                TKey pkValue;
+
+                T last = GetAll().LastOrDefault() ?? new T();
+                GetPrimaryKey(last, out pkValue);
+
+                int nextInt = Convert.ToInt32(pkValue) + 1;
+                return (TKey) Convert.ChangeType(nextInt, typeof (TKey));
+            }
+
+            throw new InvalidOperationException(
+                "Primary key could not be generated.  This only works for GUID and Int32.");
+        }
+    }
+}
---
+++ SharpRepository.Db4o/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharpRepository.Db4o")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("26f03070-2502-4e3f-9f4e-f047a032606b")]
