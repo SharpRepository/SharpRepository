5c855151180dd10e3705210b6f32bf681c7b55f0
bgriswold <bgriswold@gmail.com>
2012-06-29 16:06:31 -0700

    Reapplied Jeff's container naming updates.

--- SharpRepository.Db4oRepository/Db4ORepositoryBase.cs
+++ SharpRepository.Db4oRepository/Db4ORepositoryBase.cs
@@ -1,107 +1,3 @@
-<<<<<<< HEAD
-using System;
-using System.Linq;
-using Db4objects.Db4o;
-using Db4objects.Db4o.Linq;
-using SharpRepository.Repository;
-using SharpRepository.Repository.Caching;
-using SharpRepository.Repository.FetchStrategies;
-
-namespace SharpRepository.Db4oRepository
-{
-    public class Db4oRepositoryBase<T, TKey> : LinqRepositoryBase<T, TKey> where T : class, new()
-    {
-        private IObjectContainer _documentStore;
-
-        internal Db4oRepositoryBase(string storagePath, ICachingStrategy<T, TKey> cachingStrategy = null)
-            : base(cachingStrategy)
-        {
-            Initialize(storagePath);
-        }
-
-        private void Initialize(string storagePath)
-        {
-            _documentStore = Db4oEmbedded.OpenFile(Db4oEmbedded.NewConfiguration(), storagePath);
-        }
-
-        protected override IQueryable<T> BaseQuery(IFetchStrategy<T> fetchStrategy = null)
-        {
-            return _documentStore.AsQueryable<T>();
-        }
-
-        protected override T GetQuery(TKey key)
-        {
-            return _documentStore.AsQueryable<T>().FirstOrDefault(x => MatchOnPrimaryKey(x, key));
-        }
-
-        private bool MatchOnPrimaryKey(T item, TKey keyValue)
-        {
-            TKey value;
-            return GetPrimaryKey(item, out value) && keyValue.Equals(value);
-        }
-
-        protected override void AddItem(T entity)
-        {
-            TKey id;
-
-            if (GetPrimaryKey(entity, out id) && Equals(id, default(TKey)))
-            {
-                id = GeneratePrimaryKey();
-                SetPrimaryKey(entity, id);
-            }
-
-            _documentStore.Store(entity);
-        }
-
-        protected override void DeleteItem(T entity)
-        {
-            _documentStore.Delete(entity);
-        }
-
-        protected override void UpdateItem(T entity)
-        {
-            _documentStore.Store(entity);
-        }
-
-        protected override void SaveChanges()
-        {
-            _documentStore.Commit();
-        }
-
-        public override void Dispose()
-        {
-            _documentStore.Close();
-            if (_documentStore != null)
-                _documentStore.Dispose();
-        }
-
-        private TKey GeneratePrimaryKey()
-        {
-            if (typeof(TKey) == typeof(Guid))
-            {
-                return (TKey)Convert.ChangeType(Guid.NewGuid(), typeof(TKey));
-            }
-
-            if (typeof(TKey) == typeof(string))
-            {
-                return (TKey)Convert.ChangeType(Guid.NewGuid().ToString(), typeof(TKey));
-            }
-
-            if (typeof (TKey) == typeof (Int32))
-            {
-                TKey pkValue;
-
-                T last = GetAll().LastOrDefault() ?? new T();
-                GetPrimaryKey(last, out pkValue);
-
-                int nextInt = Convert.ToInt32(pkValue) + 1;
-                return (TKey) Convert.ChangeType(nextInt, typeof (TKey));
-            }
-
-            throw new InvalidOperationException("Primary key could not be generated. This only works for GUID, Int32 and String.");
-        }
-    }
-=======
 using System;
 using System.Linq;
 using Db4objects.Db4o;
@@ -180,9 +76,14 @@ namespace SharpRepository.Db4oRepository
 
         private TKey GeneratePrimaryKey()
         {
-            if (typeof (TKey) == typeof (Guid))
+            if (typeof(TKey) == typeof(Guid))
             {
-                return (TKey) Convert.ChangeType(Guid.NewGuid(), typeof (TKey));
+                return (TKey)Convert.ChangeType(Guid.NewGuid(), typeof(TKey));
+            }
+
+            if (typeof(TKey) == typeof(string))
+            {
+                return (TKey)Convert.ChangeType(Guid.NewGuid().ToString(), typeof(TKey));
             }
 
             if (typeof (TKey) == typeof (Int32))
@@ -192,13 +93,11 @@ namespace SharpRepository.Db4oRepository
                 T last = GetAll().LastOrDefault() ?? new T();
                 GetPrimaryKey(last, out pkValue);
 
-                var nextInt = Convert.ToInt32(pkValue) + 1;
+                int nextInt = Convert.ToInt32(pkValue) + 1;
                 return (TKey) Convert.ChangeType(nextInt, typeof (TKey));
             }
 
-            throw new InvalidOperationException(
-                "Primary key could not be generated.  This only works for GUID and Int32.");
+            throw new InvalidOperationException("Primary key could not be generated. This only works for GUID, Int32 and String.");
         }
     }
->>>>>>> 0a143f0c61de4566b1786818a192e584efea49f7
 }
