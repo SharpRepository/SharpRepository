d321d678b6225fa261708ba4fdcaeed82664d9e0
Jeff Treuting <jeff@truburn.net>
2012-09-25 14:24:43 -0700

    Updated summary reference from Ef to Ef5

--- SharpRepository.Ef5Repository/Ef5Repository.cs
+++ SharpRepository.Ef5Repository/Ef5Repository.cs
@@ -12,7 +12,7 @@ namespace SharpRepository.Ef5Repository
     public class Ef5Repository<T, TKey> : Ef5RepositoryBase<T, TKey> where T : class, new()
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="EfRepository&lt;T, TKey&gt;"/> class.
+        /// Initializes a new instance of the <see cref="Ef5Repository&lt;T, TKey&gt;"/> class.
         /// </summary>
         /// <param name="dbContext">The Entity Framework DbContext.</param>
         /// <param name="cachingStrategy">The caching strategy to use.  Defaults to <see cref="NoCachingStrategy&lt;T, TKey&gt;" /></param>
@@ -29,7 +29,7 @@ namespace SharpRepository.Ef5Repository
     public class Ef5Repository<T> : Ef5RepositoryBase<T, int> where T : class, new()
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="EfRepository&lt;T&gt;"/> class.
+        /// Initializes a new instance of the <see cref="Ef5Repository&lt;T&gt;"/> class.
         /// </summary>
         /// <param name="dbContext">The Entity Framework DbContext.</param>
         /// <param name="cachingStrategy">The caching strategy to use.  Defaults to <see cref="NoCachingStrategy&lt;T, TKey&gt;" /></param>
