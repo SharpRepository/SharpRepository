82bfbf66df1b556be4c7437648470096e3882a74
Jeff Treuting <jeff@truburn.net>
2012-10-05 17:10:55 -0700

    Created power shell script to automate nuget builds
    It's convention based.  It goes through each sub-directory in /nuget/
    and updates the version in the nuspec file based on the nuspec.template,
    then uses that new file to create a new package and puts that file into
    the proper nuget directory.  Then we will upload through the website as
    normal.

---
+++ nuget/nuget-build.ps1
@@ -0,0 +1,47 @@
+# Go through each directory in the current directory and build the nuget package for it
+# The user will be prompted for what version number to use
+
+function Get-Directory([string[]]$path, [string[]]$include, [switch]$recurse) 
+{ 
+    Get-ChildItem -Path $path -Include $include -Recurse:$recurse | `
+         Where-Object { $_.PSIsContainer } 
+} 
+
+Write-Host "This will build all of the nuget packages in this directory"
+Write-Host ""
+
+$cur_directory = $(Get-Location)
+$version_holder = "<version></version>"
+$new_version = Read-Host "What version number should we use? (e.g. 1.0.0.3)"
+
+Write-Host ""
+
+$directories = Get-Directory $(Get-Location), "SharpRepository.*" -recurse
+
+foreach($directory in $directories)
+{
+	$dir_fullname = $directory.fullname
+
+	Write-Host "Processing " $dir_fullname
+	
+	$template_path = ($directory.fullname + "\" + $directory + ".nuspec.template")
+	$nuspec_path = ($directory.fullname + "\" + $directory + ".nuspec")
+	
+	# update the version number
+	((Get-Content $template_path) -creplace $version_holder, ("<version>" + $new_version + "</version>")) | Set-Content $nuspec_path
+	
+	# copy the nuspec file over to the main project directory because I only know how to create the nuget package from that diretory, but that's just me i think :)
+	Copy-Item $nuspec_path "..\$directory\$directory.nuspec"
+	
+	# build the nuget package
+	#& "C\NuGet\nuget.exe pack ..\$directory\$directory.csproj -Prop Configuration=Release"
+	Invoke-Expression "& `"nuget.exe`" pack ..\$directory\$directory.csproj -Prop Configuration=Release"
+	
+	# move the nuget package back to the nuget directories
+	Move-Item "$cur_directory\$directory.$new_version.nupkg" "$dir_fullname\$directory.$new_version.nupkg"
+	
+	# delete the nuspec file from the project directory
+	Remove-Item "..\$directory\$directory.nuspec"
+}
+
+Read-Host -prompt "Finished.  Press Enter key to exit"
