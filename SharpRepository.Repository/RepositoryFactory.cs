ce227ee424d7d27d8b8b8f7f43f9f63b20f5554c
Jeff Treuting <jeff@truburn.net>
2012-12-04 20:50:21 -0800

    SharpRepositoryConfigurationAttribute
    If you mark a class with this attribute you can give it the name of a
    repository name from the configuration to use specifically for this
    class.  This allows you to hook up all your classes via SM to use the
    Ef5Repository, let's say, and then hook up the class SearchItem to use
    RavenDbRepository so your IRepository<SearchItem, string> will get
    populated from SM with a RavenDbRepositoruy while all the rest will use
    the Ef5Repository.  Adds another way of configuring and overriding when
    using IOC.

--- SharpRepository.Repository/RepositoryFactory.cs
+++ SharpRepository.Repository/RepositoryFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using SharpRepository.Repository.Configuration;
+using SharpRepository.Repository.Helpers;
 
 namespace SharpRepository.Repository
 {
@@ -43,11 +44,23 @@ namespace SharpRepository.Repository
 
         public static IRepository<T, TKey> GetInstance<T, TKey>(ISharpRepositoryConfiguration configuration, string repositoryName=null) where T : class, new()
         {
+            if (String.IsNullOrEmpty(repositoryName))
+            {
+                // if no specific repository is provided then check to see if the SharpRepositoryConfigurationAttribute is used
+                repositoryName = GetAttributeRepositoryName(typeof (T));
+            }
+
             return configuration.GetInstance<T, TKey>(repositoryName);
         }
 
         public static object GetInstance(Type entityType, Type keyType, ISharpRepositoryConfiguration configuration, string repositoryName = null)
         {
+            if (String.IsNullOrEmpty(repositoryName))
+            {
+                // if no specific repository is provided then check to see if the SharpRepositoryConfigurationAttribute is used
+                repositoryName = GetAttributeRepositoryName(entityType);
+            }
+
             var method = typeof(ISharpRepositoryConfiguration).GetMethod("GetInstance", new [] { typeof(string) });
             var genericMethod = method.MakeGenericMethod(entityType, keyType);
             return genericMethod.Invoke(configuration, new object[] { repositoryName });
@@ -60,6 +73,15 @@ namespace SharpRepository.Repository
                 throw new ConfigurationErrorsException("Section " + sectionName + " is not found.");
 
             return section;
+        }
+
+        private static string GetAttributeRepositoryName(Type entityType)
+        {
+            var attribute = entityType.GetOneAttribute<SharpRepositoryConfigurationAttribute>();
+            if (attribute == null)
+                return null;
+
+            return attribute.RepositoryName;
         }
     }
 }
