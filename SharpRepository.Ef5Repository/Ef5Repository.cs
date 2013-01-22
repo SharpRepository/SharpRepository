e97e3a7fe2ffcb065b76774ec8910451d1de6f2f
Jeff Treuting <jeff@truburn.net>
2013-01-22 14:08:33 -0800

    EF5 spike integration for compound key
    I made a one off integration test for EF5 since it's not easy to
    integrate this in to the other integration tests at this point because
    the difference in the interfaces used.  Hopefully we can figure a way to
    clean this up a bit.  But overall the POC is working I think.
    
    Still need implementations for the real caching strategies, that comes
    next.

--- SharpRepository.Ef5Repository/Ef5Repository.cs
+++ SharpRepository.Ef5Repository/Ef5Repository.cs
@@ -4,6 +4,14 @@ using SharpRepository.Repository.Caching;
 
 namespace SharpRepository.Ef5Repository
 {
+    public class Ef5Repository<T, TKey, TKey2> : Ef5CompoundKeyRepositoryBase<T, TKey, TKey2> where T : class, new()
+    {
+        public Ef5Repository(DbContext dbContext, ICompoundKeyCachingStrategy<T, TKey, TKey2> cachingStrategy = null)
+            : base(dbContext, cachingStrategy)
+        {
+        }
+    }
+
     /// <summary>
     /// Entity Framework repository layer
     /// </summary>
---
+++ SharpRepository.Tests.Integration/Spikes/Ef5CompoundKeySpikes.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using NUnit.Framework;
+using SharpRepository.Ef5Repository;
+using SharpRepository.Repository;
+using SharpRepository.Tests.Integration.Data;
+using SharpRepository.Tests.Integration.TestObjects;
+using Should;
+
+namespace SharpRepository.Tests.Integration.Spikes
+{
+    [TestFixture]
+    public class Ef5CompoundKeySpikes
+    {
+         ICompoundKeyRepository<User, string, int> _repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            var dbPath = EfDataDirectoryFactory.Build();
+            Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
+            _repository = new Ef5Repository<User, string, int>(new TestObjectEntities("Data Source=" + dbPath));
+        }
+
+        [Test]
+        public void CompoundKeyRepository_Should_Work()
+        {
+            _repository.Add(new User { Username = "jeff", Age = 21, FullName = "Jeff - 21" });
+            _repository.Add(new User { Username = "jeff", Age = 31, FullName = "Jeff - 31" });
+            _repository.Add(new User { Username = "jeff", Age = 41, FullName = "Jeff - 41" });
+
+            _repository.Add(new User { Username = "ben", Age = 31, FullName = "Ben - 31" });
+            _repository.Add(new User { Username = "ben", Age = 41, FullName = "Ben - 41" });
+            _repository.Add(new User { Username = "ben", Age = 51, FullName = "Ben - 51" });
+
+            _repository.Get("jeff", 31).FullName.ShouldEqual("Jeff - 31");
+            _repository.Get("ben", 31).FullName.ShouldEqual("Ben - 31");
+            _repository.Get("jeff", 41).FullName.ShouldEqual("Jeff - 41");
+
+            _repository.FindAll(x => x.Age == 31).Count().ShouldEqual(2);
+        }
+    }
+}
