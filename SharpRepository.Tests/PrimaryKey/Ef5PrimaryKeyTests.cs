e1b800365995f79ba20c427943015c6bd0e44055
Jeff Treuting <jeff@truburn.net>
2013-02-17 13:13:02 -0800

    Added 2 unit tests for new EF5 and MongoDb PK overrides

---
+++ SharpRepository.Tests/PrimaryKey/Ef5PrimaryKeyTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using System.Reflection;
+using NUnit.Framework;
+using SharpRepository.Repository.Caching;
+using SharpRepository.Tests.TestObjects.PrimaryKeys;
+using Should;
+
+namespace SharpRepository.Tests.PrimaryKey
+{
+    [TestFixture]
+    public class Ef5PrimaryKeyTests
+    {
+        [Test]
+        public void Should_Return_KeyInt2_Property()
+        {
+            var repos = new TestEf5Repository<ObjectKeys, int>(new DbContext("test"));
+            var propInfo = repos.TestGetPrimaryKeyPropertyInfo();
+
+            propInfo.PropertyType.ShouldEqual(typeof(int));
+            propInfo.Name.ShouldEqual("KeyInt2");
+        }
+    }
+
+    internal class TestEf5Repository<T, TKey> : Ef5Repository.Ef5Repository<T, TKey> where T : class, new()
+    {
+        public TestEf5Repository(DbContext dbContext, ICachingStrategy<T, TKey> cachingStrategy = null) : base(dbContext, cachingStrategy)
+        {
+        }
+
+        public PropertyInfo TestGetPrimaryKeyPropertyInfo()
+        {
+            return GetPrimaryKeyPropertyInfo();
+        }
+    }
+}
---
+++ SharpRepository.Tests/PrimaryKey/MongoDbPrimaryKeyTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+using SharpRepository.Tests.TestObjects.PrimaryKeys;
+using Should;
+
+namespace SharpRepository.Tests.PrimaryKey
+{
+    [TestFixture]
+    public class MongoDbPrimaryKeyTests
+    {
+        [Test]
+        public void Should_Return_KeyInt1_Property()
+        {
+            var repos = new TestMongoDbRepository<ObjectKeys, int>();
+            var propInfo = repos.TestGetPrimaryKeyPropertyInfo();
+
+            propInfo.PropertyType.ShouldEqual(typeof(int));
+            propInfo.Name.ShouldEqual("KeyInt1");
+        }
+    }
+
+    internal class TestMongoDbRepository<T, TKey> : MongoDbRepository.MongoDbRepository<T, TKey> where T : class, new()
+    {
+        public PropertyInfo TestGetPrimaryKeyPropertyInfo()
+        {
+            return GetPrimaryKeyPropertyInfo();
+        }
+    }
+}
