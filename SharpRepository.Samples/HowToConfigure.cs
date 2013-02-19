7e74aa71188b829e518c0993562f5c34ac0c2505
Jeff Treuting <jeff@truburn.net>
2013-02-18 16:52:53 -0800

    HowToConfigure sample
    Just a link to the blog post about creating repositories and using
    configuration files, etc.
    
    Fixes #46

---
+++ SharpRepository.Samples/HowToConfigure.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace SharpRepository.Samples
+{
+    [TestFixture]
+    public class HowToConfigure
+    {
+        /*
+         *  Configuring and create an instance of your repository is covered in this blog post for now:
+         *    http://www.fairwaytech.com/2013/02/sharprepository-configuration/
+         * 
+         * 
+         */
+    }
+}
