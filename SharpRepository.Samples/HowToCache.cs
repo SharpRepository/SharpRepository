f794d2fc3bf60897f6af1dec952f29df0143c0b4
Jeff Treuting <jeff@truburn.net>
2013-04-26 18:03:14 -0700

    HowToCache for now pointing to the github wiki page I made

---
+++ SharpRepository.Samples/HowToCache.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace SharpRepository.Samples
+{
+    [TestFixture]
+    public class HowToCache
+    {
+        /*
+         * Details on caching can be found here: https://github.com/SharpRepository/SharpRepository/wiki/Caching
+         */
+    }
+}
