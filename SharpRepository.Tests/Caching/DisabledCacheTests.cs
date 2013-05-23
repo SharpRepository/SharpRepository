e420ecadd4668551b223af4d83f9ee6c8fad0d98
Jeff Treuting <jeff@truburn.net>
2013-05-22 19:49:55 -0700

    Unused setup in unit test

--- SharpRepository.Tests/Caching/DisabledCacheTests.cs
+++ SharpRepository.Tests/Caching/DisabledCacheTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.Caching;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SharpRepository.InMemoryRepository;
 using SharpRepository.Repository.Caching;
 using SharpRepository.Tests.TestObjects;
@@ -10,17 +9,6 @@ namespace SharpRepository.Tests.Caching
     [TestFixture]
     public class DisabledCacheTests
     {
-        [SetUp]
-        public void Setup()
-        {
-            // need to clear out the InMemory cache before each test is run so that each is independent and won't effect the next one
-            var cache = MemoryCache.Default;
-            foreach (var item in cache)
-            {
-                cache.Remove(item.Key);
-            }
-        }
-
         [Test]
         public void Using_DisableCaching_Should_Disable_Cache_Inside_Using_Block()
         {
