da07ed064187b5d50f0442df57469eb8d67448d1
Jeff Treuting <jeff@truburn.net>
2013-08-10 12:03:50 -0700

    Xml integration test directory forgotten

--- SharpRepository.Tests.Integration/Data/XmlDataDirectoryFactory.cs
+++ SharpRepository.Tests.Integration/Data/XmlDataDirectoryFactory.cs
@@ -7,8 +7,14 @@ namespace SharpRepository.Tests.Integration.Data
         public static string Build(string type)
         {
             var dataDirectory = DataDirectoryHelper.GetDataDirectory();
-            string xmlDataDirectoryPath = Path.Combine(dataDirectory, @"Xml");
-            string file = string.Format("{0}\\{1}.xml", xmlDataDirectoryPath, type);
+            var xmlDataDirectoryPath = Path.Combine(dataDirectory, @"Xml");
+
+            if (!Directory.Exists(xmlDataDirectoryPath))
+            {
+                Directory.CreateDirectory(xmlDataDirectoryPath);
+            }
+
+            var file = string.Format("{0}\\{1}.xml", xmlDataDirectoryPath, type);
             
             if (File.Exists(file))
             {
