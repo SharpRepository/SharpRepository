f1fd2e9c56bf5d3158acc6250a91d8e0436b9599
Jeff Treuting <jeff@truburn.net>
2013-10-09 20:37:37 -0700

    Added Ioc projects for MVC and WebApi
    They use DependencyResolver.Current and
    GlobalConfiguration.Configuration.DependencyResolver respectively

---
+++ SharpRepository.Ioc.Mvc/MvcDependencyResolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using SharpRepository.Repository.Ioc;
+
+namespace SharpRepository.Ioc.Mvc
+{
+    public class MvcDependencyResolver : IRepositoryDependencyResolver
+    {
+        public T Resolve<T>()
+        {
+            return DependencyResolver.Current.GetService<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            return DependencyResolver.Current.GetService(type);
+        }
+    }
+}
