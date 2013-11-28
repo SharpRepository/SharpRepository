e10523a5b218b12873863ea14cda7a18185fd9cd
Jeff Treuting <jeff@truburn.net>
2013-11-27 22:32:20 -0800

    Added AzureBlobRepository

---
+++ SharpRepository.AzureBlobRepository/AzureBlobRepository.cs
@@ -0,0 +1,22 @@
+﻿using SharpRepository.Repository.Caching;
+
+namespace SharpRepository.AzureBlobRepository
+{
+    public class AzureBlobRepository<T, TKey> : AzureBlobRepositoryBase<T, TKey> where T: class, new()
+    {
+        public AzureBlobRepository(string connectionString, string containerName = null, bool createIfNotExists = true, ICachingStrategy<T, TKey> cachingStrategy = null)
+            : base(connectionString, containerName, createIfNotExists, cachingStrategy)
+        {
+            
+        }
+    }
+
+    public class AzureBlobRepository<T> : AzureBlobRepositoryBase<T, string> where T : class, new()
+    {
+        public AzureBlobRepository(string connectionString, string containerName = null, bool createIfNotExists = true, ICachingStrategy<T, string> cachingStrategy = null)
+            : base(connectionString, containerName, createIfNotExists, cachingStrategy)
+        {
+
+        }
+    }
+}
---
+++ SharpRepository.Tests/Spikes/AzureBlobSpikes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+using NUnit.Framework;
+using SharpRepository.AzureBlobRepository;
+using Should;
+
+namespace SharpRepository.Tests.Spikes
+{
+    [TestFixture]
+    public class AzureBlobSpikes
+    {
+        [Test]
+        public void TestAzureBlobGet()
+        {
+//            var repo = new AzureBlobRepository<TestBlob, Guid>("UseDevelopmentStorage=true", "course2015", false);
+//            var item = repo.Get(Guid.Parse("722b6bea-d609-48e0-a4af-3ed0f5160ad9"));
+//
+//            item.ShouldNotBeNull();
+
+            var storageAccount = Microsoft.WindowsAzure.Storage.CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("course2015");
+
+            var blob = container.GetBlockBlobReference("722b6bea-d609-48e0-a4af-3ed0f5160ad9");
+            var text = blob.DownloadText();
+
+            text.ShouldNotBeNull();
+        }
+    }
+
+    public class TestBlob
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
