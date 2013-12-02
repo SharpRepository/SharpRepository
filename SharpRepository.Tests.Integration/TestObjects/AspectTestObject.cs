ed87e59da7819f0ebde7c4399e5cbaa405cd1060
Jeff Treuting <jeff@truburn.net>
2013-12-01 23:21:51 -0800

    Made Audit spike a little better

--- SharpRepository.Tests.Integration/TestObjects/AspectTestObject.cs
+++ SharpRepository.Tests.Integration/TestObjects/AspectTestObject.cs
@@ -3,35 +3,45 @@ using SharpRepository.Repository.Aspects;
 
 namespace SharpRepository.Tests.Integration.TestObjects
 {
-    public class CurrentDateTimeSetterAttribute : RepositoryActionBaseAttribute
+    public class AuditAttribute : RepositoryActionBaseAttribute
     {
         public override bool OnAddExecuting<T, TKey>(T entity, RepositoryActionContext<T, TKey> context)
         {
-            // this is hacky but just for testing
             var tmp = entity as IAuditable;
 
             if (tmp != null)
             {
-                if (tmp.Created == DateTime.MinValue)
-                {
-                    tmp.Created = DateTime.UtcNow;
-                }
+                tmp.Created = DateTime.UtcNow;
+            }
+
+            return true;
+        }
+
+        public override bool OnUpdateExecuting<T, TKey>(T entity, RepositoryActionContext<T, TKey> context)
+        {
+            var tmp = entity as IAuditable;
+
+            if (tmp != null)
+            {
+                tmp.Modified = DateTime.UtcNow;
             }
 
             return true;
         }
     }
 
-    [CurrentDateTimeSetter]
+    [Audit]
     public class AspectTestObject : IAuditable
     {
         public int AspectTestObjectId { get; set; }
         public string Name { get; set; }
         public DateTime Created { get; set; }
+        public DateTime? Modified { get; set; }
     }
 
     public interface IAuditable
     {
         DateTime Created { get; set; }
+        DateTime? Modified { get; set; }
     }
 }
