d44a3d86fc939f9fe89377c02d9ec68a090e5344
Troy Robinson <bbqchickenrobot@gmail.com>
2013-12-30 16:38:50 -0800

    Added SimpleInjector IoC project

---
+++ SharpRepository.Ioc.SimpleInjector/SimpleInjectorDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using SharpRepository.Repository.Ioc;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpRepository.Ioc.SimpleInjector
+{
+    public class SimpleInjectorDependencyResolver : BaseRepositoryDependencyResolver
+    {
+        private readonly Container _container;
+        public SimpleInjectorDependencyResolver(Container container)
+        {
+            _container = container;
+        }
+
+        protected override T ResolveInstance<T>() 
+        {
+            return (T)_container.GetInstance(typeof(T));
+        }
+
+        protected override object ResolveInstance(Type type)
+        {
+            return _container.GetInstance(type);
+        }
+
+        // may not operate correctly if T is a primitive type
+        protected T ResolveInstanceInternal<T>() where T : class
+        {
+            return _container.GetInstance<T>();
+        }
+    }
+}
---
+++ SharpRepository.Ioc.SimpleInjector/SimpleInjectorRepositoryExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpRepository.Repository;
+using SharpRepository.Repository.Configuration;
+using SimpleInjector;
+
+namespace SharpRepository.Ioc.SimpleInjector
+{
+    public static class SimpleInjectorRepositoryExtensions
+    {
+        public static void BindSharpRepository(this Container container, string repositoryName = null)
+        {
+            //container.RegisterWithContext<IRepository<Object>>(context =>
+            //                                                   {
+            //                                                       var genericArgs = context.ImplementationType.GetGenericArguments();
+
+            //                                                       return RepositoryFactory.GetInstance(genericArgs[0],
+            //                                                           repositoryName);
+            //                                                   });
+
+            container.ResolveUnregisteredType += (s, e) =>
+            {
+                var type = e.UnregisteredServiceType;
+                if (type.IsGenericType)
+                {
+                    var args = type.GetGenericArguments();
+                    var typedef = type.GetGenericTypeDefinition();
+                    if (typedef == typeof (IRepository<>))
+                    {
+                        e.Register(() => RepositoryFactory.GetInstance(args[0], repositoryName));
+                    }
+                    else if (typedef == typeof (IRepository<,>))
+                    {
+                        e.Register(() => RepositoryFactory.GetInstance(args[0], args[1], repositoryName));
+                    }
+                    else if (typedef == typeof (ICompoundKeyRepository<,,>))
+                    {
+                        e.Register(() => RepositoryFactory.GetInstance(args[0], args[1], args[2], repositoryName));
+                    }
+                }
+            };
+            
+            // todo - find out how to implement this method
+            //throw new NotImplementedException("SimpleInjector Extension methods are not implemented");
+        }
+
+        public static void BindSharpRepository(this Container container, ISharpRepositoryConfiguration configuration)
+        {
+
+            container.ResolveUnregisteredType += (s, e) =>
+            {
+                var type = e.UnregisteredServiceType;
+                if (type.IsGenericType)
+                {
+                    var args = type.GetGenericArguments();
+                    var typedef = type.GetGenericTypeDefinition();
+                    if (typedef == typeof(IRepository<>))
+                    {
+                        e.Register(() => RepositoryFactory.GetInstance(args[0], configuration));
+                    }
+                    else if (typedef == typeof(IRepository<,>))
+                    {
+                        e.Register(() => RepositoryFactory.GetInstance(args[0], args[1], configuration));
+                    }
+                    else if (typedef == typeof(ICompoundKeyRepository<,,>))
+                    {
+                        e.Register(() => RepositoryFactory.GetInstance(args[0], args[1], args[2], configuration));
+                    }
+                }
+            };
+
+            // todo - find out how to implement this method
+            //throw new NotImplementedException("SimpleInjector Extension methods are not implemented");
+        }
+    }
+}
