72104b4e9bca74e5a4649e4aa516cd0e9e0671a9
Jeff Treuting <jeff@truburn.net>
2014-01-11 11:29:20 -0800

    HowToUseCaching
    just a reference to the GitHub wiki for now but will help if users are
    viewing the samples to get more info

---
+++ SharpRepository.Samples/HowToUseCaching.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpRepository.Samples
+{
+    // For now a reference to some more information
+    // Reference: https://github.com/SharpRepository/SharpRepository/wiki/Caching
+}
