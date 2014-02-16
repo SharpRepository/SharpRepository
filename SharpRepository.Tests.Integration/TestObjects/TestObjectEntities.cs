4aa83183a76e5274b40aaa7753d3f0977dbcccc3
Nemonic <gitnem@nym.hush.com>
2014-02-16 04:19:10 +0000

    EntityFramework nullable partition key caching tests.

--- SharpRepository.Tests.Integration/TestObjects/TestObjectEntities.cs
+++ SharpRepository.Tests.Integration/TestObjects/TestObjectEntities.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 
 namespace SharpRepository.Tests.Integration.TestObjects
 {
@@ -6,17 +7,28 @@ namespace SharpRepository.Tests.Integration.TestObjects
     {
         public TestObjectEntities(string connectionString) : base(connectionString)
         {
-            
+
         }
 
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<PhoneNumber> PhoneNumbers { get; set; }
         public DbSet<EmailAddress> EmailAddresses { get; set; }
+        public DbSet<Node> Nodes { get; set; }
 
         // set the Compound Key for the User object
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasKey(u => new {u.Username, u.Age});
+
+            modelBuilder.Entity<Node>()
+                .HasKey(n => n.Id)
+                .Property(n => n.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            modelBuilder.Entity<Node>()
+                .HasOptional(n => n.Parent)
+                .WithMany()
+                .HasForeignKey(n => n.ParentId);
         }
     }
 }
