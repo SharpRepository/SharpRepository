773ad321dcd4af2b86c08b4d2ad00e8236c34e74
Jeff Treuting <jeff@truburn.net>
2014-03-27 16:15:31 -0700

    Azure blob configuration
    Allow configuration via config file

---
+++ SharpRepository.AzureBlobRepository/AzureBlobConfigRepositoryFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SharpRepository.Repository;
+using SharpRepository.Repository.Configuration;
+
+namespace SharpRepository.AzureBlobRepository
+{
+    public class AzureBlobConfigRepositoryFactory : ConfigRepositoryFactory
+    {
+        public AzureBlobConfigRepositoryFactory(IRepositoryConfiguration config)
+            : base(config)
+        {
+        }
+
+        public override IRepository<T> GetInstance<T>()
+        {
+            throw new NotImplementedException("AzureBlobRepository does not support using IRepository<T> directly to reference a IRepository<T, string>");           
+        }
+
+        public override IRepository<T, TKey> GetInstance<T, TKey>()
+        {
+            return new AzureBlobRepository<T, TKey>(RepositoryConfiguration["connectionString"], RepositoryConfiguration["container"]);
+        }
+
+        public override ICompoundKeyRepository<T, TKey, TKey2> GetInstance<T, TKey, TKey2>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SharpRepository.AzureBlobRepository/AzureBlobRepositoryConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SharpRepository.Repository.Configuration;
+
+namespace SharpRepository.AzureBlobRepository
+{
+    public class AzureBlobRepositoryConfiguration : RepositoryConfiguration
+    {
+        public AzureBlobRepositoryConfiguration(string name) : this(name, null, null)
+        {
+        }
+
+        public AzureBlobRepositoryConfiguration(string name, string connectionString)
+            : this(name, connectionString, null)
+        {
+        }
+
+        public AzureBlobRepositoryConfiguration(string name, string connectionString, string container, bool createIfNotExists = false, string cachingStrategy = null, string cachingProvider = null)
+            : base(name)
+        {
+            ConnectionString = connectionString;
+            Container = container;
+            CachingStrategy = cachingStrategy;
+            CachingProvider = cachingProvider;
+            Factory = typeof(AzureBlobConfigRepositoryFactory);
+        }
+
+        public string ConnectionString
+        {
+            set { Attributes["connectionString"] = value; }
+        }
+
+        public string Container
+        {
+            set { Attributes["container"] = String.IsNullOrEmpty(value) ? null : value; }
+        }
+    }
+}
