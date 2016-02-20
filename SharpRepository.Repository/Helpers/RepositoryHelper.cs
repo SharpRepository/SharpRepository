0e78a64fa3e6aed97c9209f195d8bf550cd95820
Jeff Treuting <jtreuting@fairwaytech.com>
2016-02-20 15:24:39 -0800

    Updated CompoundKeyRepositoryBase to handle IncludePaths on the GetAll methods

---
+++ SharpRepository.Repository/Helpers/RepositoryHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+using SharpRepository.Repository.FetchStrategies;
+
+namespace SharpRepository.Repository.Helpers
+{
+    public static class RepositoryHelper
+    {
+        public static IFetchStrategy<T> BuildFetchStrategy<T>(params string[] includePaths)
+        {
+            var fetchStrategy = new GenericFetchStrategy<T>();
+            foreach (var path in includePaths)
+            {
+                fetchStrategy.Include(path);
+            }
+            return fetchStrategy;
+        }
+
+        public static IFetchStrategy<T> BuildFetchStrategy<T>(params Expression<Func<T, object>>[] includePaths)
+        {
+            var fetchStrategy = new GenericFetchStrategy<T>();
+            foreach (var path in includePaths)
+            {
+                fetchStrategy.Include(path);
+            }
+            return fetchStrategy;
+        }
+    }
+}
