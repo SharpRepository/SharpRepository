c532a192005c5016f14fd0e63116a775272ce142
Eivind Gussiås Løkseth <eloekset@msn.com>
2016-07-14 22:31:14 +0200

    Added reference to Microsoft.Extensions.Configuration.Xml for .NET Core.

--- SharpRepository.Repository/Configuration/CachingProviderCollection.cs
+++ SharpRepository.Repository/Configuration/CachingProviderCollection.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+#if NET451
 using System.Configuration;
+#elif NETSTANDARD1_4
+using Microsoft.Extensions.Configuration.Xml;
+#endif
 using System.Linq;
 
 namespace SharpRepository.Repository.Configuration
