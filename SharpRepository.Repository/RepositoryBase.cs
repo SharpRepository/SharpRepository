2c4edbf6bba47af9f87f3a98a15814fa541f9b33
primez <vladnleonov@gmail.com>
2016-10-13 09:23:35 +0300

    Removed prefiltering by enabled aspects on creation

--- SharpRepository.Repository/RepositoryBase.cs
+++ SharpRepository.Repository/RepositoryBase.cs
@@ -31,7 +31,6 @@ namespace SharpRepository.Repository
             _typeName = entityType.Name;
 
             _aspects = entityType.GetAllAttributes<RepositoryActionBaseAttribute>(inherit: true)
-                .Where(x => x.Enabled)
                 .OrderBy(x => x.Order)
                 .ToDictionary(a => a.GetType().FullName, a => a);
 
