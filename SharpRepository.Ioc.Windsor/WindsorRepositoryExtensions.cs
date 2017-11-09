0cae4c18d546e57a862bd73cf3d86038b6e2a149
Omar Piani <omar.piani@gmail.com>
2017-11-09 15:46:31 +0100

    Windsor covers all factory types

--- SharpRepository.Ioc.Windsor/WindsorRepositoryExtensions.cs
+++ SharpRepository.Ioc.Windsor/WindsorRepositoryExtensions.cs
@@ -23,11 +23,25 @@ namespace SharpRepository.Ioc.Windsor
                     return RepositoryFactory.GetInstance(genericArgs[0], genericArgs[1], configuration, repositoryName);
                 }));
 
+            container.Register(Component.For(typeof(ICompoundKeyRepository<>)).UsingFactoryMethod((c, t) =>
+            {
+                var genericArgs = t.GenericArguments;
+
+                return RepositoryFactory.GetCompoundKeyInstance(genericArgs[0], configuration, repositoryName);
+            }));
+
             container.Register(Component.For(typeof(ICompoundKeyRepository<,,>)).UsingFactoryMethod((c, t) =>
             {
                 var genericArgs = t.GenericArguments;
 
                 return RepositoryFactory.GetInstance(genericArgs[0], genericArgs[1], genericArgs[2], configuration, repositoryName);
+            }));
+
+            container.Register(Component.For(typeof(ICompoundKeyRepository<,,,>)).UsingFactoryMethod((c, t) =>
+            {
+                var genericArgs = t.GenericArguments;
+
+                return RepositoryFactory.GetInstance(genericArgs[0], genericArgs[1], genericArgs[2], genericArgs[3], configuration, repositoryName);
             }));
         }
     }
