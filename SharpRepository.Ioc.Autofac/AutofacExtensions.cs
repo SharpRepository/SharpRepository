e9ab3c8a0b70d0f116ef055f8978ff6cc74a0cf2
Omar Piani <omar.piani@gmail.com>
2018-03-21 16:16:30 +0100

    New package version for Autofac

--- SharpRepository.Ioc.Autofac/AutofacExtensions.cs
+++ SharpRepository.Ioc.Autofac/AutofacExtensions.cs
@@ -9,6 +9,13 @@ namespace SharpRepository.Ioc.Autofac
 {
     public static class AutofacExtensions
     {
+        /// <summary>
+        /// Registers in autofac container all IRepository and ICompoundKeyRepository resolutions.
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="configuration"></param>
+        /// <param name="repositoryName"></param>
+        /// <param name="lifetimeScopeTag">Accepts any MatchingScopeLifetimeTags scope enum tag</param>
         public static void RegisterSharpRepository(this ContainerBuilder container, ISharpRepositoryConfiguration configuration, string repositoryName = null, params object[] lifetimeScopeTag)
         {
             container.RegisterSource(new RepositoryRegistrationSource(configuration, repositoryName, lifetimeScopeTag));
