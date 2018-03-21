f85c16dba02c9926fef2e9df28015101320205e4
Omar Piani <omar.piani@gmail.com>
2018-03-21 16:12:07 +0100

    Autofac sample in MVC5 project

--- SharpRepository.Samples.MVC5/Global.asax.cs
+++ SharpRepository.Samples.MVC5/Global.asax.cs
@@ -1,4 +1,11 @@
-﻿using SharpRepository.Ioc.Mvc;
+﻿using Autofac;
+using Autofac.Core.Lifetime;
+using Autofac.Integration.Mvc;
+using Microsoft.Extensions.Configuration;
+using SharpRepository.Ioc.Autofac;
+using SharpRepository.Ioc.Mvc;
+using SharpRepository.Repository;
+using SharpRepository.Repository.Configuration;
 using SharpRepository.Samples.MVC5.Models;
 using StructureMap;
 using StructureMap.Pipeline;
@@ -23,6 +30,26 @@ namespace SharpRepository.Samples.MVC5
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             // MvcDependencyResolver.ForRepositoriesUseSharpRepository("repository.json", "sharpRepository", "efConnectionString"); // holds connection string on repository.json
             MvcDependencyResolver.ForRepositoriesUseSharpRepository("repository.json", "sharpRepository", lifecycle: Lifecycles.Unique); // no connection string on repository.json
+            
+            var config = new ConfigurationBuilder()
+              .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+              .AddJsonFile("repository.json")
+              .Build();
+
+            var section = config.GetSection("sharpRepository");
+            ISharpRepositoryConfiguration configuration = RepositoryFactory.BuildSharpRepositoryConfiguation(section);
+
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterSharpRepository(configuration, null, MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
+            builder.RegisterType<ContactsDbContext>().As<DbContext>().InstancePerRequest();
+            var container = builder.Build();
+            
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
         }
     }
 
