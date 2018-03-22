b81747a59cb7eedd78a7373acf7314176aa4ba62
Omar Piani <omar.piani@gmail.com>
2018-03-22 13:14:01 +0100

    Added unity ioc container

---
+++ SharpRepository.Ioc.Unity/UnityRepositoryDependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SharpRepository.Repository.Ioc;
+using Unity;
+
+namespace SharpRepository.Ioc.Unity
+{
+    public class UnityRepositoryDependencyResolver : BaseRepositoryDependencyResolver
+    {
+        private readonly IUnityContainer _container;
+        public UnityRepositoryDependencyResolver(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        protected override T ResolveInstance<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        protected override object ResolveInstance(Type type)
+        {
+            return _container.Resolve(type);
+        }
+    }
+}
