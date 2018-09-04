609079ef614b0b5522923041ba670c201cf29621
Dillon Adams <dillon-a@hotmail.com>
2018-09-04 15:15:47 -0500

    Adding base class to CompoundKeyRepositoryBase class with two keys

--- SharpRepository.Repository/CompoundKeyRepositoryBase.cs
+++ SharpRepository.Repository/CompoundKeyRepositoryBase.cs
@@ -201,34 +201,26 @@ namespace SharpRepository.Repository
         }
     }
 
-    public abstract partial class CompoundKeyRepositoryBase<T, TKey, TKey2> : ICompoundKeyRepository<T, TKey, TKey2> where T : class
+    public abstract partial class CompoundKeyRepositoryBase<T, TKey, TKey2> 
+        : CompoundKeyRepositoryBaseCommon<T>, ICompoundKeyRepository<T, TKey, TKey2> where T : class
     {
         // the caching strategy used
         private ICompoundKeyCachingStrategy<T, TKey, TKey2> _cachingStrategy;
 
         // the query manager uses the caching strategy to determine if it should check the cache or run the query
         private CompoundKeyQueryManager<T, TKey, TKey2> _queryManager;
-
-        // just the type name, used to find the primary key if it is [TypeName]Id
-        private readonly string _typeName;
-        protected string TypeName
-        {
-            get { return _typeName; }
-        }
-        
-        public bool CacheUsed
+                
+        public override bool CacheUsed
         {
             get { return _queryManager.CacheUsed; }
         }
 
-        public IBatch<T> BeginBatch()
+        public override IBatch<T> BeginBatch()
         {
             // Return the privately scoped batch via the publicly available interface. 
             // This ensures that a repository alone can initiate a new batch.
             return new Batch(this);
         }
-      
-        private bool BatchMode { get; set; }
 
         protected CompoundKeyRepositoryBase(ICompoundKeyCachingStrategy<T, TKey, TKey2> cachingStrategy = null)
         {
@@ -239,7 +231,6 @@ namespace SharpRepository.Repository
             }
 
             CachingStrategy = cachingStrategy ?? new NoCachingStrategy<T, TKey, TKey2>();
-            _typeName = typeof (T).Name;
         }
 
         public ICompoundKeyCachingStrategy<T, TKey, TKey2> CachingStrategy 
@@ -251,39 +242,8 @@ namespace SharpRepository.Repository
                 _queryManager = new CompoundKeyQueryManager<T, TKey, TKey2>(_cachingStrategy);
             }
         } 
-
-        public abstract IQueryable<T> AsQueryable();
-
-        // These are the actual implementation that the derived class needs to implement
-        protected abstract IQueryable<T> GetAllQuery(IFetchStrategy<T> fetchStrategy);
-        protected abstract IQueryable<T> GetAllQuery(IQueryOptions<T> queryOptions, IFetchStrategy<T> fetchStrategy);
-
-        public IEnumerable<T> GetAll()
-        {
-            return GetAll((IQueryOptions<T>)null, (IFetchStrategy<T>)null);
-        }
-
-        public IEnumerable<T> GetAll(IFetchStrategy<T> fetchStrategy)
-        {
-            return GetAll((IQueryOptions<T>)null, fetchStrategy);
-        }
-
-        public IEnumerable<T> GetAll(params string[] includePaths)
-        {
-            return GetAll(RepositoryHelper.BuildFetchStrategy<T>(includePaths));
-        }
-
-        public IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includePaths)
-        {
-            return GetAll(RepositoryHelper.BuildFetchStrategy<T>(includePaths));
-        }
-
-        public IEnumerable<T> GetAll(IQueryOptions<T> queryOptions)
-        {
-            return GetAll(queryOptions, (IFetchStrategy<T>)null);
-        }
-
-        public IEnumerable<T> GetAll(IQueryOptions<T> queryOptions, IFetchStrategy<T> fetchStrategy)
+        
+        public override IEnumerable<T> GetAll(IQueryOptions<T> queryOptions, IFetchStrategy<T> fetchStrategy)
         {
             return _queryManager.ExecuteGetAll(
                 () => GetAllQuery(queryOptions, fetchStrategy).ToList(),
@@ -292,42 +252,7 @@ namespace SharpRepository.Repository
                 );
         }
 
-        public IEnumerable<T> GetAll(IQueryOptions<T> queryOptions, params string[] includePaths)
-        {
-            return GetAll(queryOptions, RepositoryHelper.BuildFetchStrategy<T>(includePaths));
-        }
-
-        public IEnumerable<T> GetAll(IQueryOptions<T> queryOptions, params Expression<Func<T, object>>[] includePaths)
-        {
-            return GetAll(queryOptions, RepositoryHelper.BuildFetchStrategy(includePaths));
-        }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector)
-        {
-            return GetAll(selector, (IQueryOptions<T>)null, (IFetchStrategy<T>)null);
-        }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions)
-        {
-            return GetAll(selector, queryOptions, (IFetchStrategy<T>)null);
-        }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, IFetchStrategy<T> fetchStrategy)
-        {
-            return GetAll(selector, null, fetchStrategy);
-        }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, params string[] includePaths)
-        {
-            return GetAll(selector, RepositoryHelper.BuildFetchStrategy<T>(includePaths));
-        }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, params Expression<Func<T, object>>[] includePaths)
-        {
-            return GetAll(selector, RepositoryHelper.BuildFetchStrategy(includePaths));
-        }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions, IFetchStrategy<T> fetchStrategy)
+        public override IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions, IFetchStrategy<T> fetchStrategy)
         {
             if (selector == null) throw new ArgumentNullException("selector");
 
@@ -337,23 +262,6 @@ namespace SharpRepository.Repository
                 queryOptions
                 );
         }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions, params string[] includePaths)
-        {
-            return GetAll(selector, queryOptions, RepositoryHelper.BuildFetchStrategy<T>(includePaths));
-        }
-
-        public IEnumerable<TResult> GetAll<TResult>(Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions, params Expression<Func<T, object>>[] includePaths)
-        {
-            return GetAll(selector, queryOptions, RepositoryHelper.BuildFetchStrategy(includePaths));
-        }
-
-        // These are the actual implementation that the derived class needs to implement
-        protected abstract T GetQuery(TKey key, TKey2 key2);
-
-        public abstract IRepositoryQueryable<TResult> Join<TJoinKey, TInner, TResult>(IRepositoryQueryable<TInner> innerRepository, Expression<Func<T, TJoinKey>> outerKeySelector, Expression<Func<TInner, TJoinKey>> innerKeySelector, Expression<Func<T, TInner, TResult>> resultSelector)
-            where TInner : class
-            where TResult : class;
 
         public T Get(TKey key, TKey2 key2)
         {
@@ -420,11 +328,7 @@ namespace SharpRepository.Repository
             }
         }
 
-        // These are the actual implementation that the derived class needs to implement
-        protected abstract IQueryable<T> FindAllQuery(ISpecification<T> criteria);
-        protected abstract IQueryable<T> FindAllQuery(ISpecification<T> criteria, IQueryOptions<T> queryOptions);
-
-        public IEnumerable<T> FindAll(ISpecification<T> criteria, IQueryOptions<T> queryOptions = null)
+        public override IEnumerable<T> FindAll(ISpecification<T> criteria, IQueryOptions<T> queryOptions = null)
         {
             if (criteria == null) throw new ArgumentNullException("criteria");
 
@@ -436,7 +340,7 @@ namespace SharpRepository.Repository
                 );
         }
 
-        public IEnumerable<TResult> FindAll<TResult>(ISpecification<T> criteria, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions = null)
+        public override IEnumerable<TResult> FindAll<TResult>(ISpecification<T> criteria, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions = null)
         {
             if (criteria == null) throw new ArgumentNullException("criteria");
 
@@ -448,26 +352,7 @@ namespace SharpRepository.Repository
                 );
         }
 
-        public IEnumerable<T> FindAll(Expression<Func<T, bool>> predicate, IQueryOptions<T> queryOptions = null)
-        {
-            if (predicate == null) throw new ArgumentNullException("predicate");
-
-            return FindAll(new Specification<T>(predicate), queryOptions);
-        }
-
-        public IEnumerable<TResult> FindAll<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions = null)
-        {
-            if (predicate == null) throw new ArgumentNullException("predicate");
-            if (selector == null) throw new ArgumentNullException("selector");
-
-            return FindAll(new Specification<T>(predicate), selector, queryOptions);
-        }
-
-        // These are the actual implementation that the derived class needs to implement
-        protected abstract T FindQuery(ISpecification<T> criteria);
-        protected abstract T FindQuery(ISpecification<T> criteria, IQueryOptions<T> queryOptions);
-
-        public T Find(ISpecification<T> criteria, IQueryOptions<T> queryOptions = null)
+        public override T Find(ISpecification<T> criteria, IQueryOptions<T> queryOptions = null)
         {
             if (criteria == null) throw new ArgumentNullException("criteria");
 
@@ -479,7 +364,7 @@ namespace SharpRepository.Repository
                 );
         }
 
-        public TResult Find<TResult>(ISpecification<T> criteria, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions = null)
+        public override TResult Find<TResult>(ISpecification<T> criteria, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions = null)
         {
             if (criteria == null) throw new ArgumentNullException("criteria");
             if (selector == null) throw new ArgumentNullException("selector");
@@ -500,126 +385,12 @@ namespace SharpRepository.Repository
                 );
         }
 
-        public bool Exists(ISpecification<T> criteria)
-        {
-            return TryFind(criteria, out T entity);
-        }
-
-        public bool TryFind(ISpecification<T> criteria, out T entity)
-        {
-            return TryFind(criteria, (IQueryOptions<T>)null, out entity);
-        }
-
-        public bool TryFind(ISpecification<T> criteria, IQueryOptions<T> queryOptions, out T entity)
-        {
-            entity = null;
-
-            try
-            {
-                entity = Find(criteria, queryOptions);
-                return entity != null;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        public bool TryFind<TResult>(ISpecification<T> criteria, Expression<Func<T, TResult>> selector, out TResult entity)
-        {
-            return TryFind(criteria, selector, null, out entity);
-        }
-
-        public bool TryFind<TResult>(ISpecification<T> criteria, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions, out TResult entity)
-        {
-            entity = default(TResult);
-
-            try
-            {
-                entity = Find(criteria, selector, queryOptions);
-                return !entity.Equals(default(TResult));
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        public T Find(Expression<Func<T, bool>> predicate, IQueryOptions<T> queryOptions = null)
-        {
-            if (predicate == null) throw new ArgumentNullException("predicate");
-
-            return Find(new Specification<T>(predicate), queryOptions);
-        }
-
-        public TResult Find<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions = null)
-        {
-            if (predicate == null) throw new ArgumentNullException("predicate");
-            if (selector == null) throw new ArgumentNullException("selector");
-
-            return Find(new Specification<T>(predicate), selector, queryOptions);
-        }
-
-        public bool Exists(Expression<Func<T, bool>> predicate)
-        {
-            return TryFind(predicate, out T entity);
-        }
-
-        public bool TryFind(Expression<Func<T, bool>> predicate, out T entity)
-        {
-            return TryFind(predicate, (IQueryOptions<T>)null, out entity);
-        }
-
-        public bool TryFind(Expression<Func<T, bool>> predicate, IQueryOptions<T> queryOptions, out T entity)
-        {
-            entity = null;
-
-            try
-            {
-                entity = Find(predicate, queryOptions);
-                return entity != null;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        public bool TryFind<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, out TResult entity)
-        {
-            return TryFind(predicate, selector, null, out entity);
-        }
-
-        public bool TryFind<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> selector, IQueryOptions<T> queryOptions, out TResult entity)
-        {
-            entity = default(TResult);
-
-            try
-            {
-                entity = Find(predicate, selector, queryOptions);
-                return !entity.Equals(default(TResult));
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        // This is the actual implementation that the derived class needs to implement
-        protected abstract void AddItem(T entity);
-
-        public void Add(T entity)
+        public override void Add(T entity)
         {
             if (entity == null) throw new ArgumentNullException("entity");
 
-            ProcessAdd(entity, BatchMode);
-        }
-
-        // used from the Add method above and the Save below for the batch save
-        private void ProcessAdd(T entity, bool batchMode)
-        {
             AddItem(entity);
-            if (batchMode) return;
+            if (BatchMode) return;
 
             Save();
 
@@ -627,44 +398,17 @@ namespace SharpRepository.Repository
                 _queryManager.OnItemAdded(key, key2, entity);
         }
 
-        public void Add(IEnumerable<T> entities)
-        {
-            if (entities == null) throw new ArgumentNullException("entities");
-
-            foreach (var entity in entities)
-            {
-                Add(entity);
-            }
-        }
-
-        // This is the actual implementation that the derived class needs to implement
-        protected abstract void DeleteItem(T entity);
-
         public void Delete(T entity)
         {
             if (entity == null) throw new ArgumentNullException("entity");
 
-            ProcessDelete(entity, BatchMode);
-        }
-
-        // used from the Delete method above and the Save below for the batch save
-        private void ProcessDelete(T entity, bool batchMode)
-        {
             DeleteItem(entity);
-            if (batchMode) return;
+            if (BatchMode) return;
 
             Save();
 
             if (GetPrimaryKey(entity, out TKey key, out TKey2 key2))
                 _queryManager.OnItemDeleted(key, key2, entity);
-        }
-
-        public void Delete(IEnumerable<T> entities)
-        {
-            foreach (var entity in entities)
-            {
-                Delete(entity);
-            }
         }
 
         public void Delete(TKey key, TKey2 key2)
@@ -676,31 +420,12 @@ namespace SharpRepository.Repository
             Delete(entity);
         }
 
-        public void Delete(Expression<Func<T, bool>> predicate)
-        {
-            Delete(new Specification<T>(predicate));
-        }
-
-        public void Delete(ISpecification<T> criteria)
-        {
-            Delete(FindAll(criteria));
-        }
-
-        // This is the actual implementation that the derived class needs to implement
-        protected abstract void UpdateItem(T entity);
-
-        public void Update(T entity)
+        public override void Update(T entity)
         {
             if (entity == null) throw new ArgumentNullException("entity");
 
-            ProcessUpdate(entity, BatchMode);
-        }
-
-        // used from the Update method above and the Save below for the batch save
-        private void ProcessUpdate(T entity, bool batchMode)
-        {
             UpdateItem(entity);
-            if (batchMode) return;
+            if (BatchMode) return;
 
             Save();
 
@@ -708,27 +433,12 @@ namespace SharpRepository.Repository
                 _queryManager.OnItemUpdated(key, key2, entity);
         }
 
-        public void Update(IEnumerable<T> entities)
-        {
-            if (entities == null) throw new ArgumentNullException("entities");
-
-            foreach (var entity in entities)
-            {
-                Update(entity);
-            }
-        }
-
-        protected abstract void SaveChanges();
-
         private void Save()
         {
             SaveChanges();
             
             _queryManager.OnSaveExecuted(); 
         }
-
-        
-        public abstract void Dispose();
 
         protected virtual bool GetPrimaryKey(T entity, out TKey key, out TKey2 key2) 
         {
@@ -784,14 +494,6 @@ namespace SharpRepository.Repository
                 );
 
             return new Specification<T>(lambda).And(lambda2);
-        }
-
-        protected virtual PropertyInfo[] GetPrimaryKeyPropertyInfo()
-        {
-            var type = typeof(T);
-            var properties = type.GetTypeInfo().DeclaredProperties;
-
-            return properties.Where(x => x.HasAttribute<RepositoryPrimaryKeyAttribute>()).OrderBy(x => x.GetOneAttribute<RepositoryPrimaryKeyAttribute>().Order).ToArray();
         }
     }
 
